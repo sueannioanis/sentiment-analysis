568d945630cfa0f49fa2908938e3d43208681260
Sue Ann Ioanis <sueann.ioanis@gmail.com>
2019-09-16 15:52:26 -0400

    Clean up js

--- server.js
+++ server.js
@@ -1,48 +1,54 @@
-'use strict';
-const express = require('express');
-const bodyParser = require('body-parser');
-const request = require('request');
+"use strict";
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 const app = express();
+const watson = require("watson-developer-cloud");
+
+const ID = "<@BOT_ID_GOES_HERE>";
+const THRESHOLD = 0.55;
+
+const CONFIG = {
+  url: "https://gateway.watsonplatform.net/tone-analyzer/api",
+  username: "USER_NAME_GOES_HERE",
+  password: "PASSWORD_GOES_HERE",
+  version: "v3",
+  version_date: "2016-05-19"
+};
+
+const EMOTIONS = {
+  anger: "😠",
+  disgust: "🤢",
+  fear: "😱",
+  joy: "😄",
+  sadness: "😞"
+};
+
+const GREETING =
+  "Hi! My name is Watson. Tag me if you'd like to analyze your text.";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const server = app.listen(4000);
+app.listen(4000);
 
-const emotions = {
-  anger : '😠',
-  disgust : '🤢',
-  fear : '😱',
-  joy : '😄',
-  sadness : '😞'
-}
+let tone_analyzer = watson.tone_analyzer(CONFIG);
 
-const confidencethreshold = 0.55;
+let latestTimestamp = +new Date() / 1000;
 
-const watson = require('watson-developer-cloud');
-let tone_analyzer = watson.tone_analyzer({
-  url: 'https://gateway.watsonplatform.net/tone-analyzer/api',
-  username: "USER_NAME_GOES_HERE",
-  password: "PASSWORD_GOES_HERE",
-  version: 'v3',
-  version_date: '2016-05-19'
-});
-
-let latestTimestamp = + new Date()/1000;
-
-app.post('/event', (req, res) => {
-  switch(req.body.type) {
-    case 'url_verification':
+app.post("/event", (req, res) => {
+  switch (req.body.type) {
+    case "url_verification":
       res.send(req.body.challenge);
       break;
-    case 'event_callback':
-      if (req.body.event.ts > latestTimestamp && !req.body.event.bot_id) {
-        const {text, channel, ts} = req.body.event;
-        const textToAnalyze = text.replace('<@BOT_ID_GOES_HERE>','');
+    case "event_callback":
+      const { text, channel, ts, bot_id } = req.body.event;
+      if (ts > latestTimestamp && !bot_id) {
+        const textToAnalyze = text.replace(ID, "");
         if (!textToAnalyze) {
-          postMessage(channel, 'Hi! My name is Watson. Tag me if you\'d like to analyze your text.')
+          postMessage(channel, GREETING);
         } else if (text) {
-          analyzeTone(channel, textToAnalyze)
+          analyzeTone(channel, textToAnalyze);
         }
         latestTimestamp = ts;
       }
@@ -51,38 +57,46 @@ app.post('/event', (req, res) => {
 });
 
 function analyzeTone(channel, text) {
-  tone_analyzer.tone({text}, (err, tone) => {
-    let emotes = '';
-    let emotionString = 'Tone Analysis:\n';
-    tone.document_tone.tone_categories.forEach((tonecategory) => {
-      if(tonecategory.category_id === 'emotion_tone') {
-        tonecategory.tones.forEach((emotion) => {
-          if(emotion.score >= confidencethreshold && emotes.indexOf(emotions[emotion.tone_id])) {
-            emotes += emotions[emotion.tone_id]
-          }
-          emotionString += emotion.tone_name + ': ' + emotion.score*100 + '%\n'
-        })
-      } else {
-        tonecategory.tones.forEach((emotion) => {
-          emotionString += emotion.tone_name + ': ' + emotion.score*100 + '%\n'
-        })
-      }
-    })
-    postMessage(channel, 'Emoting ... ' + (emotes || '😐') + '\n\n' + emotionString)
+  tone_analyzer.tone({ text }, (err, tone) => {
+    let emotes = "";
+    let emotionString = "Tone Analysis:\n";
+    tone.document_tone.tone_categories.forEach(generateString);
+    postMessage(
+      channel,
+      "Emoting ... " + (emotes || "😐") + "\n\n" + emotionString
+    );
   });
+}
+
+function generateString(tonecategory) {
+  if (tonecategory.category_id === "emotion_tone") {
+    tonecategory.tones.forEach(emotion => {
+      if (
+        emotion.score >= THRESHOLD &&
+        emotes.indexOf(EMOTIONS[emotion.tone_id])
+      ) {
+        emotes += EMOTIONS[emotion.tone_id];
+      }
+      emotionString += emotion.tone_name + ": " + emotion.score * 100 + "%\n";
+    });
+  } else {
+    tonecategory.tones.forEach(emotion => {
+      emotionString += emotion.tone_name + ": " + emotion.score * 100 + "%\n";
+    });
+  }
 }
 
 function postMessage(channel, message) {
   let options = {
-    method: 'POST',
-    url: 'https://slack.com/api/chat.postMessage',
+    method: "POST",
+    url: "https://slack.com/api/chat.postMessage",
     form: {
-      token: 'SLACK_OAUTH_TOKEN_GOES_HERE', // Your Slack OAuth token
+      token: "SLACK_OAUTH_TOKEN_GOES_HERE", // Your Slack OAuth token
       channel,
       text: message
     }
   };
-  request(options, (error, response, body) => {
-    console.log(response.body)
-  })
+  request(options, response => {
+    console.log(response.body);
+  });
 }
